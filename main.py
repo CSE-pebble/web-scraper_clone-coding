d3a840af7e7bd3e379c4bef05c3ecffe99a95d7f
CSE-pebble <CSE.pebble@gmail.com>
2022-09-07 08:17:19 +0900

    Add exception processing

--- main.py
+++ main.py
@@ -4,4 +4,7 @@ base_url = "https://weworkremotely.com/remote-jobs/search?utf8=%E2%9C%93&term="
 search_term = "python"
 
 response = get(f"{base_url}{search_term}")
-print(response.text)
+if response.status_code != 200:
+  print("Can't request website")
+else:
+  print(response.text)
