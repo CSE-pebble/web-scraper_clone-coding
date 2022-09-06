ad39e7a9ed120b17582c65312630ecd97ac356da
CSE-pebble <CSE.pebble@gmail.com>
2022-09-07 08:25:42 +0900

    Add web scraping function
    by BeautifulSoup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from requests import get
+from bs4 import BeautifulSoup
 
 base_url = "https://weworkremotely.com/remote-jobs/search?utf8=%E2%9C%93&term="
 search_term = "python"
@@ -7,4 +8,5 @@ response = get(f"{base_url}{search_term}")
 if response.status_code != 200:
   print("Can't request website")
 else:
-  print(response.text)
+  soup = BeautifulSoup(response.text, "html.parser") # HTML 다 가져옴
+  jobs = soup.find_all("section", class_="jobs") # HTML에서 class명이 jobs인 모든 sections 태그 가져오기
