394e161338e4f5f3c70b6ae4f247d2a1c9a3331b
CSE-pebble <CSE.pebble@gmail.com>
2022-09-11 18:13:47 +0900

    Fix 403 error by selenium

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from requests import get
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 from extractors.wwr import extract_wwr_jobs
 
-jobs = extract_wwr_jobs("python")
-print(jobs)
+options = Options()
+browser = webdriver.Chrome(options=options)
+
+base_url = 'https://kr.indeed.com/jobs?q='
+keyword = 'python'
+
+browser.get(f"{base_url}{keyword}")
+
+soup = BeautifulSoup(browser.page_source, "html.parser")
