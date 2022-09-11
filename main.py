a296efb1bdefd071fb607a41d12f97296bd7d912
CSE-pebble <CSE.pebble@gmail.com>
2022-09-11 18:42:30 +0900

    Add finding position function

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ browser = webdriver.Chrome(options=options)
 base_url = 'https://kr.indeed.com/jobs?q='
 keyword = 'python'
 
-browser.get(f"{base_url}{keyword}")
+browser.get(f"{base_url}{keyword}&limit=50")
 
 soup = BeautifulSoup(browser.page_source, "html.parser")
 job_list = soup.find("ul", class_="jobsearch-ResultsList")
@@ -18,6 +18,6 @@ jobs = job_list.find_all("li", recursive=False)
 for job in jobs :
   zone = job.find("div", class_="mosaic-zone")
   if zone == None: 
-    print("job li")
-  else:
-    print("mosaic li")
+    anchor = job.select_one("h2 a")
+    title = anchor['aria-label']
+    link = anchor['href']
